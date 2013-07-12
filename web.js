4049f156b996068f18684df3669901e7b3b5cec1
rogwang <rogwang@gmail.com>
2013-07-12 01:02:41 +0000

    Modified web.js to read from index.html.

--- web.js
+++ web.js
@@ -5,16 +5,22 @@ var express = require("express");
 //var app = express(express.logger());
 var app = express.createServer(express.logger());
 
+
 fs.readFile('index.html', "utf-8", function (err, data) {
   if (err) throw err;
-//  console.log(data);
-});
 
 app.get('/', function(request, response) {
+  console.log(data);
   response.send(data);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
+//console.log(data);
   console.log("Listening on " + port);
 });
+
+
+
+});
+
